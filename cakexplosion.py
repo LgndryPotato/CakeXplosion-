c01cb908e4166325637e746483de2d87c688654f
Tristan Laur <144528378+LgndryPotato@users.noreply.github.com>
2023-12-20 21:52:05 +0200

    Update cakexplosion.py

--- cakexplosion.py
+++ cakexplosion.py
@@ -112,14 +112,10 @@ def uus_sõna():
     url = "https://kodu.ut.ee/~tristanl/sõnad.html"
     sõna = random.choice(suvaline_sõna_veebist(url))
     kook = random.choice(kookide_ikoonide_list)
-
-    if kook in [kookide_ikoonid_valged1, kookide_ikoonid_valged2, kookide_ikoonid_valged3,kookide_ikoonid_valged4]: värv = must
-    else: värv = valge
     return {
         "sõna": sõna,
         "x": random.randint(0, ekraani_laius - sõna_suurus * len(sõna)),
         "y": 0,
-        "värv": värv,
         "ikoon": kook
     }
 
@@ -135,7 +131,7 @@ def main_menu():
     # nupud
     start_button = pygame.Rect(ekraani_laius//2 - 200//2, ekraan_korgus//2 - 100//2 - 50, 200, 100)
     leaderboard_button = pygame.Rect(ekraani_laius//2 - 200//2, ekraan_korgus//2 + 10, 200, 100)
-    start_tekst = font_main_start.render("Start", True, must)
+    start_tekst = font_main_start.render("Alusta", True, must)
     leaderboard_tekst = font_main_leaderboard.render("Edetabel", True, must)
     while menu:
         for event in pygame.event.get():
@@ -206,6 +202,7 @@ def display_leaderboard():
 
 def loo_sprinkild(x, y):
     ekraan.blit(sprinkles_ikoon, (x, y))
+
 def game_loop():
     # muutujad
     elud = 3
@@ -258,8 +255,13 @@ def game_loop():
         # vaatab kas sisend ja sõnad ekraanil samad
         for sõna in sõnad_ekraanil:
             sõna["y"] += kiirus
-            text_surface = font.render(sõna["sõna"], True, sõna["värv"])
             ekraan.blit(sõna["ikoon"], (sõna["x"] - 50, sõna["y"] - 75))
+            text_surface = font.render(sõna["sõna"], True, valge)
+            border_surface = font.render(sõna["sõna"], True, must)
+
+            # Draw the border
+            for dx, dy in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
+                ekraan.blit(border_surface, (sõna["x"] + dx, sõna["y"] + dy))
             ekraan.blit(text_surface, (sõna["x"], sõna["y"]))
 
             # kui kirjutad sõna mis ekraanil siis automaatselt võtab selle ära ei pea enterit vajutama
@@ -276,13 +278,11 @@ def game_loop():
         if sprinkles_start_time is not None and pygame.time.get_ticks() - sprinkles_start_time < 200:
             ekraan.blit(sprinkles_ikoon, (sprinkles_position[0] - 50, sprinkles_position[1] - 75))
 
+        roosa_kast = pygame.Rect(0, 670, 200, 50)
+        pygame.draw.rect(ekraan, roosa, roosa_kast)
+        input_surface = font.render(sisend, True, must)
+        ekraan.blit(input_surface, (10, ekraan_korgus - sõna_suurus))
 
-        if level == 1 or level == 3 or level == 5 or level == 7 or level == 9 or level == 11 or level == 13 or level == 15:
-            input_surface = font.render(sisend, True, valge)
-            ekraan.blit(input_surface, (10, ekraan_korgus - sõna_suurus))
-        else:
-            input_surface = font.render(sisend, True, must)
-            ekraan.blit(input_surface, (10, ekraan_korgus - sõna_suurus))
         if level == 1 or level == 3 or level == 5 or level == 7 or level == 9 or level == 11 or level == 13 or level == 15:
             skoor_surface = font.render("Skoor: {}".format(skoor), True, valge)
             ekraan.blit(skoor_surface, (10, 10))
